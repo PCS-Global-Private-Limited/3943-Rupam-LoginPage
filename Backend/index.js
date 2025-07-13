7a8566cff5f92225536505a464182d39e45f90c2
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-07-14 01:31:42 +0530

    Added the functionality to encript the password & implimented login logic

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,8 @@ const User = require("./models/SingUp");
 const app = express();
 const PORT = 5080;
 const MONGO = "mongodb://localhost:27017/contactdb";
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 app.use(express.json());
 app.use(cors());
@@ -31,7 +33,8 @@ app.post("/signup", async (req, res) => {
     if (user) {
       return res.status(409).json({ exists: true, message: "User already exists with this email or mobile number" });
     }
-    const newUser = new User({ userName, emailId, mobileNumber, password });
+    const hashed = await bcrypt.hash(password, 10);
+    const newUser = new User({ userName, emailId, mobileNumber, password : hashed });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
@@ -39,6 +42,18 @@ app.post("/signup", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
+app.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
+  const user = await User.findOne({ userName });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+  const token = jwt.sign({ userId: user._id }, "secret", { expiresIn: "7d" });
+  res.json({ token });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
